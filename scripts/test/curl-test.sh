3ed289f758ac0ca8acbdffccc0a52556ce271de9
tempest-code <tempest.andrew@gmail.com>
2021-01-28 19:02:53 +0000

    update curl-test to get aws service url

--- scripts/test/curl-test.sh
+++ scripts/test/curl-test.sh
@@ -1,3 +1,3 @@
-url=http://a09ccfb15dd1c4debad9fcb5bade51a9-475905179.eu-west-1.elb.amazonaws.com
+url=$(kubectl get services simple-jwt-api -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 TOKEN=`curl -d '{"email":"test@test.local","password":"Password1"}' -H "Content-Type: application/json" -X POST "$url/auth"  | jq -r '.token'`
 curl --request GET "$url/contents" -H "Authorization: Bearer $TOKEN" | jq
